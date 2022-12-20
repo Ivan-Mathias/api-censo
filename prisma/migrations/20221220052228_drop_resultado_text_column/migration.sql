7533d573260a606654dda739492530e22d76a158
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2022-12-20 02:39:18 -0300

    fix results table id

---
+++ prisma/migrations/20221220052228_drop_resultado_text_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `resposta` on the `Resultado` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Resultado" DROP COLUMN "resposta";
